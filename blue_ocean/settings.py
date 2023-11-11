204b1afa588c6b6e4f12b63b89df2f8c011c7d4e
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-11 07:15:07 +0000

    add contact to apps in settings.py and turn debug to False

--- blue_ocean/settings.py
+++ blue_ocean/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dlynch95-blueoceantrave-v5j6nx6eqnx.ws-us105.gitpod.io','8000-dlynch95-blueoceantrave-v5j6nx6eqnx.ws-us106.gitpod.io']
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'home',
     'bag',
     'products',
+    'contact',
 
     #other
     'storages',
