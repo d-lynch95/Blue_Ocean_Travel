e8f028183a217a0296eeec7be090deb53fd091c5
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-11 03:21:50 +0000

    turn off debug mode in settings file

--- blue_ocean/settings.py
+++ blue_ocean/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-dlynch95-blueoceantrave-v5j6nx6eqnx.ws-us105.gitpod.io','8000-dlynch95-blueoceantrave-v5j6nx6eqnx.ws-us106.gitpod.io']
 
@@ -82,7 +82,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'bag.contexts.bag_contents',
             ],
         },
     },
--- check.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import User
-
-admin_user = User.objects.get(username='admin')
-last_login = admin_user.last_login
-print(last_login)
