ea43ddd122693ad898ad26d48a7ab0a058ca54ef
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-11 03:13:33 +0000

    add Base_dir to settings.py file line 21

--- blue_ocean/settings.py
+++ blue_ocean/settings.py
@@ -17,7 +17,8 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
