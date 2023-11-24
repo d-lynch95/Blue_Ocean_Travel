3d6d7fe45c19c99260d9ecd3827024a4477f4de6
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-24 06:50:19 +0000

    add contact_success page and models to admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import contact_us
 
-# Register your models here.
+class ContactUsAdmin(admin.ModelAdmin):
+
+    verbose_name_plural = 'Contact Forms'
+
+    readonly_fields = ( 'full_name', 'email','phone_number',
+                        'input_text','selection',)
+
+    fields = ('full_name', 'email','phone_number',
+                'input_text','selection',)
+
+    list_display = ('full_name', 'email', 'phone_number',
+                    'input_text','selection',)
+
+admin.site.register(contact_us, ContactUsAdmin)
+
--- contact/views.py
+++ contact/views.py
@@ -9,5 +9,25 @@ def contact_us(request):
     context = {
         'contact_form': contact_form,
     }
-
     return render(request, template, context)
+
+
+def contact_success(request,):
+    """
+    Handle successful contact us form submissions
+    """
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()  # Save the form data to the backend database
+            messages.success(request, f'Your form has been submitted.')
+            return redirect('contact_success.html')  # Redirect to the success page
+    else:
+        contact_form = ContactForm()
+        messages.warning(request, f'Your form was not submitted.')
+
+    context = {
+        'contact_form': contact_form,
+    } 
+
+    return render(request, 'contact_success.html', context)
