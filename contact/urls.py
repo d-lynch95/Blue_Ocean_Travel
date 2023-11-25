6e8c48706b88ffbfe22c57f81fcf9c8567cc50ce
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-25 00:24:03 +0000

    Delete contact urls and views pages to rebuild them

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,2 @@
 from django.urls import path
 from . import views
-
-urlpatterns = [
-    path('', views.contact_us, name='contact'),
-    path('contact_success/', views.contact_success, name='success'),
-]
