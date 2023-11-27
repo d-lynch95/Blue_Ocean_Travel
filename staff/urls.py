f563bdef0380ff1f2b55fcb48cce907ae0eb40ba
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-27 06:16:57 +0000

    add views and urls to staff edit tours section

--- staff/urls.py
+++ staff/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.staff_view, name='staff'),
     path('view_forms/', views.ContactList.as_view(), name='view_forms'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 
 ]
--- staff/views.py
+++ staff/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 
+from products.models import Product
 from contact.models import contactForm
 from .forms import ProductForm
 
@@ -40,6 +41,29 @@ def add_product(request):
     template = 'staff/add_tour.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+def edit_product(request, product_id):
+    """ Edit a tour in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'staff/edit_tour.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
