fd28fd3255c8480e44b304d97caefbad0e7fb2b3
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-28 07:41:52 +0000

    change code to fix contact page

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.views import generic
+from django.views import generic, View
 from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 
 from .forms import contForm
@@ -12,23 +12,22 @@ def contact(request):
     context = {"contForm": contForm}
     return render(request, template, context)
 
-class SubmitForm(LoginRequiredMixin, generic.CreateView):
+class SubmitForm(View):
     def get(self, request):
-        cont_form = contForm()
+        form = contForm()
         context = {"contForm": form}
-        return render(request, 'contact/contact.html', context)
+        return render(request, "contact/contact.html", context)
 
     def post(self, request):
         form = contForm(request.POST)
         if form.is_valid():
-            form = form.save(commit=False)
+            instance = form.save(commit=False)
             form.user = request.user
             form.save()
             messages.success(
                 self.request,
                 f'Your contact form has been sent')
             return redirect("contact/contact_success.html")
-
         else:
             context = {"contForm": contForm}
             return render(request, 'contact/contact.html', context)
