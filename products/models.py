e1acdf26282e8d5403ff319e0eb670bc434367b1
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-28 00:51:42 +0000

    create model for users to leave reviews

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# Model to segment products by different locations
 class Region(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -10,6 +12,7 @@ class Region(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+#Model to view different products
 class Product(models.Model):
     region = models.ForeignKey('Region', null=True, blank=True, on_delete=models.SET)
     name = models.CharField(max_length=254)
@@ -22,3 +25,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Model to allow users to leave reviews for products
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=3)
+    content = models.TextField()
+    created_by = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
