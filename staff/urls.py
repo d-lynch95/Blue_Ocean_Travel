c7005f3846141a87d2fb6f88cf4c483693c0c6a6
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-28 08:11:18 +0000

    add functionality to allow staff to delete tours

--- staff/urls.py
+++ staff/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('view_forms/', views.ContactList.as_view(), name='view_forms'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
