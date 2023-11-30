d4f75b296fd37d4dc3aa75df66d1d5461d5b5544
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-30 14:14:32 +0000

    create code to add products to the wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.WishList.as_view(), name="wishlist"),
+    path('addwish/<int:item_id>/', views.add_to_wishlist, name='addwish'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from wishlist.models import WishlistItem
+from products.models import Product
 
 class WishList(LoginRequiredMixin,  ListView):
     """
@@ -16,3 +17,16 @@ class WishList(LoginRequiredMixin,  ListView):
     model = WishlistItem
 
 
+def add_to_wishlist(request, item_id):
+    product = Product.objects.get(pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+    list = request.session.get('list', {})
+
+    if item_id in list.keys():
+        messages.success(request, f'{product.name} is already on your wishlist')
+    else:
+        list[item_id] = product
+        messages.success(request, f'Added {product.name} to your wishlist')
+
+    request.session['list'] = list
+    return redirect(redirect_url)
