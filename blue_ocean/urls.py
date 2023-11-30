7a62d917817a5289dd01f394c5daccfc32dcedf6
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-30 13:31:29 +0000

    fix code to urls and models for wishlist, create template

--- blue_ocean/urls.py
+++ blue_ocean/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('checkout/', include('checkout.urls')),
     path('staff/', include('staff.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, View
+from django.views.generic.edit import DeleteView
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class WishList(LoginRequiredMixin, UserPassesTestMixin, ListView):
+from wishlist.models import WishlistItem
+
+class WishList(LoginRequiredMixin,  ListView):
     """
     A view that provides the wishlist of products
     """
 
     template_name = "wishlist/wishlist.html"
-    model = WishlistLine
+    model = WishlistItem
 
 
