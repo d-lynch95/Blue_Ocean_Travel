4061cdafc466985c839cc97cdbdb277533f7de0e
d-lynch95 <darraghlynch1995@gmail.com>
2023-11-30 06:41:50 +0000

    remove remove widget option from widgets.py

--- staff/widgets.py
+++ staff/widgets.py
@@ -3,7 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
     template_name = 'staff/custom_widgets_templates/custom_clearable_file_input.html'
