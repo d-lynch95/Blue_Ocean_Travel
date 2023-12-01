aaf330a8897ffa180a552161249c7c048cdd15fc
d-lynch95 <darraghlynch1995@gmail.com>
2023-12-01 08:05:22 +0000

    add provicy policy and links in footer, add link to wishlist in nav

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about_us, name='aboutus'),
+    path('privacy/', views.about_us, name='privacy'),
 ]
