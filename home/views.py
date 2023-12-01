36924a5790b42ef79c11094bf8424ca62433dff0
d-lynch95 <darraghlynch1995@gmail.com>
2023-12-01 08:12:01 +0000

    format privacy policy page and add link to footer

--- home/views.py
+++ home/views.py
@@ -7,3 +7,7 @@ def index(request):
 def about_us(request):
     """ A view to return the about us page """
     return render(request, 'home/about.html')
+
+def about_us(request):
+    """ A view to return the privacy policy """
+    return render(request, 'home/privacy.html')
